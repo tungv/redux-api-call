361ccd190c3bb01302286af93790ef6797d4b320
Tung <me@tungv.com>
2016-09-26 00:34:18 +0700

    clean up test builder

--- rollup.test.config.js
+++ rollup.test.config.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import stub from 'rollup-plugin-stub';
 import alias from 'rollup-plugin-alias';
-import async from 'rollup-plugin-async';
 import buble from 'rollup-plugin-buble';
 import multiEntry from 'rollup-plugin-multi-entry';
 
@@ -9,24 +8,17 @@ const tests = process.env.TESTS;
 const entry = tests ? tests.split(',').map(t => `src/__tests__/${t}.spec.js`) : 'src/__tests__/*.spec.js';
 
 export default {
-  entry: [
-    'node_modules/isomorphic-fetch/fetch-npm-node.js',
-    entry
-  ],
+  entry,
   dest: '.build/tests.js',
   format: 'cjs',
   plugins: [
     stub(),
-    async(),
     multiEntry(),
     buble({
       objectAssign: 'Object.assign',
       transforms: {
         generator: false,
       }
-    }),
-    alias({
-      'redux-api-call': path.resolve('./src/index.js')
     }),
   ]
 };
