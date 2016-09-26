048d9cb61877f5a9e55189aff808e3e77b92db0b
Tung <me@tungv.com>
2016-09-26 18:45:23 +0700

    exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import buble from 'rollup-plugin-buble';
+import babel from 'rollup-plugin-babel';
 import strip from 'rollup-plugin-strip';
 import uglify from 'rollup-plugin-uglify';
+import cleanup from 'rollup-plugin-cleanup';
 
 export default {
   entry: 'src/index.js',
   dest: '.build/index.js',
   format: 'cjs',
   plugins: [
-    buble({
-      objectAssign: 'Object.assign'
+    babel({
+      babelrc: false,
+      presets: ['stage-1', ['es2015', { modules: false }], 'es2016'],
+      plugins: ['external-helpers']
     }),
-    strip({
-      debugger: true,
-      functions: [ 'console.*', 'assert.*', 'debug', 'alert' ],
-    })
+    cleanup({ maxEmptyLines: 1 }),
   ]
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import middleware from './middleware';
+import makeFetchAction from './makeFetchAction';
+import { REDUCER_PATH } from './constants';
+import reducer from './reducer'
 
-export { middleware };
+const apiCallsReducer = { [REDUCER_PATH]: reducer };
+
+export { middleware, makeFetchAction, apiCallsReducer };
