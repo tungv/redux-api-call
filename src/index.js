0828ef0a30f353b5b33fb9f988e720ccbc8ca55b
Tung <me@tungv.com>
2016-09-27 13:58:36 +0700

    exports ACTIONS constants

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import middleware from './middleware';
 import makeFetchAction from './makeFetchAction';
-import { REDUCER_PATH } from './constants';
+import {
+  REDUCER_PATH,
+  ACTION_FETCH_START,
+  ACTION_FETCH_COMPLETE,
+  ACTION_FETCH_FAILURE,
+} from './constants';
 import reducer from './reducer'
 
 const reducers = { [REDUCER_PATH]: reducer };
+const ACTIONS = {
+  START: ACTION_FETCH_START,
+  COMPLETE: ACTION_FETCH_COMPLETE,
+  FAILURE: ACTION_FETCH_FAILURE,
+};
 
-export { middleware, makeFetchAction, reducers };
+export { middleware, makeFetchAction, reducers, ACTIONS };
