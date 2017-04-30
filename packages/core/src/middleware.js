0b2b92a3dec9d7d07b08db8722c5f8a8139cfa54
Tung Vu <me@tungv.com>
2017-04-30 11:19:25 +0700

    chore: convert adatpers to submodules

--- packages/core/src/middleware.js
+++ packages/core/src/middleware.js
@@ -2,8 +2,8 @@ import { Subject } from 'rxjs/Subject';
 import composeAdapters from './composeAdapters.js';
 import createActionStream from './createActionStream'
 import { CALL_API } from './constants';
-import fetch from './adapters/fetch'
-import parseJSON from './adapters/parseJSON';
+import fetch from 'redux-api-call-adapter-fetch';
+import parseJSON from 'redux-api-call-adapter-json';
 
 const defaultCreateAdapter = composeAdapters(parseJSON, fetch);
 
