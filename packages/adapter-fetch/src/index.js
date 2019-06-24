9996269d09206ad618678c817b318c79fea8b510
Phu Tang <phu.tang@tiki.vn>
2019-06-24 12:22:53 +0700

    Fix statusCode: get from resp.status

--- packages/adapter-fetch/src/index.js
+++ packages/adapter-fetch/src/index.js
@@ -15,11 +15,10 @@ export default next => async ({ endpoint, ...others }) => {
 
   const meta = {};
   resp.headers.forEach((value, key) => (meta[key] = value));
-  meta.statusCode = resp.status;
 
   if (!resp.ok) {
     const error = new Error('Bad Response');
-    error.statusCode = resp.statusCode;
+    error.statusCode = resp.status;
     error.payload = await resp.text();
     error.meta = meta;
     throw error;
